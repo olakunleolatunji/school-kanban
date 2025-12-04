cf905c88f0aa70833c81c457279c438ea67ffc7f
Olatunji Olakunle Olumuyiwa <elnuk03@gmail.com>
2025-12-04 15:58:33 +0100

    sticky header

--- run_kanban.bat
+++ run_kanban.bat
@@ -1,5 +1,5 @@
 @echo off
-:: This script launches your Kanban board automatically
+:: This script launches your Kanban board server ONLY (no browser popup)
 
 :: 1. Navigate strictly to your project folder
 cd /d "C:\Users\elnuk\Documents\kanban_project"
@@ -7,8 +7,7 @@ cd /d "C:\Users\elnuk\Documents\kanban_project"
 :: 2. Activate the virtual environment
 call venv\Scripts\activate
 
-:: 3. Open the Kanban board using your custom name
-start http://schoolkanban:5000
-
-:: 4. Run the Flask application
+:: 3. Run the Flask application
+:: The server will start, but no window will open.
+:: You can access it anytime at http://schoolkanban:5000
 python app.py
